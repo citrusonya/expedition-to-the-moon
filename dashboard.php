02be9eed9f899b96e9aaad61ae32a1761391b8be
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-05-08 23:30:04 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -37,22 +37,70 @@
             include_once './pages/oxygen generation.php';
             include_once './pages/protein_generation.php';
             include_once './pages/reports.php';
+            include_once './pages/modal.php';
         ?>
     </div>
-
     <script
         src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossorigin="anonymous">
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
     <script>
-        $('.nav-link').on('click', function(e) {
-            e.preventDefault();
-            $('.content').removeClass('active');
-            $('.nav-link').removeClass('active_link');
-            $(this).addClass('active_link');
-            let itemId = $(this).attr('data-link-id');
-            $('.content[data-content-id="' + itemId + '"]').addClass('active');
-        });
+        $(document).ready(function() {
+            $('.nav-link').on('click', function (e) {
+                e.preventDefault();
+                $('.content').removeClass('active');
+                $('.nav-link').removeClass('active_link');
+                $(this).addClass('active_link');
+                let itemId = $(this).attr('data-link-id');
+                $('.content[data-content-id="' + itemId + '"]').addClass('active');
+            });
+
+            $('.button_delete').on('click', function(){
+
+                const id = $(this).attr('id');
+
+                Swal.fire({
+                    title: 'Вы уверены, что хотите удалить запись?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Да, удалить',
+                    cancelButtonText: 'Нет'
+
+                }).then((result) => {
+
+                    if (result.isConfirmed) {
+
+                        $.ajax({
+                            url:"ajax/person_delete.php",
+                            method:"POST",
+                            data: { id:id },
+
+                            success:function(data){
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Запись удалена!',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            },
+
+                            error:function(data){
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Произошла ошибка, попробуйте снова.',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        });
+                    }
+                })
+            });
+        })
     </script>
 </body>
