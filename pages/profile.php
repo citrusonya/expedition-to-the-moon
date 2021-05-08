0c98bb309396bb3888a9015f00f944afa940d372
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-05-08 23:27:24 +0300

    Update profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -1,3 +1,7 @@
+<?php
+    require_once('./components/profile.php')
+?>
+
 <div data-content-id="profile" class="content active">
     <h2>Ваш профиль</h2>
     <table class="table">
@@ -7,14 +11,17 @@
             <th>Должность</th>
             <th>Звание</th>
             <th>Страна</th>
+            <th></th>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td><?= ucfirst($_SESSION['first_name']); ?></td>
+            <td><?= ucfirst($_SESSION['last_name']); ?></td>
+            <td><?= $person['sp_name']; ?></td>
+            <td><?= $person['rank_name']; ?></td>
+            <td><?= $person['country_name']; ?></td>
+            <td>
+                <button class="btn btn-outline-success update_person" type="button" name="update" id="<?= $_SESSION['id']; ?>"><i class="bi bi-pencil"></i></button>
+            </td>
         </tr>
     </table>
-    <a href="" class="btn btn-primary">Редактировать профиль</a>
 </div>
