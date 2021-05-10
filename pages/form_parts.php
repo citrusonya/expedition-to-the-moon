38626bbb88c4d091b894f3446f97705a6ebefae8
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-05-10 23:52:07 +0300

    Update form_parts.php

--- pages/form_parts.php
+++ pages/form_parts.php
@@ -3,8 +3,8 @@
 ?>
 
 <div class="form-group">
-    <label class="mr-sm-2" for="specialization">Должность</label>
-    <select id="specialization" class="custom-select mr-sm-2">
+    <label class="mr-sm-2" for="sp_id">Должность</label>
+    <select id="sp_id" name="sp_id" class="custom-select mr-sm-2">
         <option selected>Выбрать...</option>
         <?php foreach ($select_sp as $sel_sp): ?>
             <option value="<?=$sel_sp['sp_id']?>"><?=$sel_sp['sp_name']?></option>;
@@ -13,18 +13,18 @@
 </div>
 
 <div class="form-group">
-    <label class="mr-sm-2" for="rank">Звание</label>
-    <select id="rank" class="custom-select mr-sm-2">
+    <label class="mr-sm-2" for="rank_id">Звание</label>
+    <select id="rank_id" name="rank_id" class="custom-select mr-sm-2">
         <option selected>Выбрать...</option>
         <?php foreach ($select_rank as $sel_r): ?>
-            <option value="<?=$sel_r['rank_id']?>"><?=$sel_r['rank_name']?></option>;
+            <option data-value-id="<?=$sel_r['rank_id']?>" value="<?=$sel_r['rank_id']?>"><?=$sel_r['rank_name']?></option>;
         <?php endforeach; ?>
     </select>
 </div>
 
 <div class="form-group">
-    <label class="mr-sm-2" for="country">Страна</label>
-    <select id="country" class="custom-select mr-sm-2">
+    <label class="mr-sm-2" for="country_id">Страна</label>
+    <select id="country_id" name="country_id" class="custom-select mr-sm-2">
         <option selected>Выбрать...</option>
         <?php foreach ($select_country as $sel_c): ?>
             <option value="<?=$sel_c['country_id']?>"><?=$sel_c['country_name']?></option>;
