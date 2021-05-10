315c265ce6f6ad2b59d35ef0a043ac118986a94c
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-05-10 23:50:47 +0300

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,7 +1,7 @@
 <hr/>
   <nav class="nav justify-content-end">
     <div data-link-id="profile" class="nav-link active_link">Профиль</div>
-    <div data-link-id="team" class="nav-link">Команда</div>
+    <div data-link-id="team" class="nav-link">Экипаж</div>
     <div data-link-id="electricity" class="nav-link">Электроэнергия</div>
     <div data-link-id="water_supply" class="nav-link">Водоснабжение</div>
     <div data-link-id="oxygen_generation" class="nav-link">Кислородогенерация</div>
